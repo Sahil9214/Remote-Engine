66cb5f54811dc1d46d29147e6db5aa90213be22f
Utkarsh <utkarshsinghal369@gmail.com>
2023-12-17 22:52:44 +0530

    remote engine

--- RemoteEngine/src/routes/AllRouter.jsx
+++ RemoteEngine/src/routes/AllRouter.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import ClientDeveloperLogin from "../Components/ClientDeveloperLogin";
 import ClientSignup from "../Components/ClientSignup";
-import DeveloperSignup from "../Components/DeveloperSignup";
+
 import DeveloperPage from "../Components/DeveloperPage";
 import PrivateRouter from "../PrivateRouter/PrivateRouter";
 const AllRouter = () => {
@@ -10,7 +10,7 @@ const AllRouter = () => {
     <Routes>
       <Route path="/" element={<ClientDeveloperLogin />} />
       <Route path="/client" element={<ClientSignup />} />
-      <Route path="/developer" element={<DeveloperSignup />} />
+
       <Route path="/developerPage" element={<DeveloperPage />} />
     </Routes>
   );
